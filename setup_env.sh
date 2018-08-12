018b59bc81955c2f5a66d0b059dde2723591a954
Harish <kharishvarma@gmail.com>
2018-08-12 23:38:26 +0530

    end of asset management chapter

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
 npm init -y
-npm install --save-dev webpack webpack-cli npx
+npm install --save-dev webpack webpack-cli npx style-loader css-loader file-loader
 npm install --save lodash
 npm install
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
+import './style.css';
+import MyImage from './webpack_logo.svg';
 
 function component(){
 	let element = document.createElement('div');
-
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+	element.classList.add('hello');
+	var myLogo = new Image();
+	myLogo.src = MyImage;
+	element.appendChild(myLogo);
 	return element;
 }
 
